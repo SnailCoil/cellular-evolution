155064a21e633c785decd36b2122cb01a9628d50
SnailCoil <purpleflurm@gmail.com>
2012-09-03 19:42:47 -0600

    Moved the organisms into the environment

--- src/sherwood/cells/environment/Environment.java
+++ src/sherwood/cells/environment/Environment.java
@@ -1,7 +1,14 @@
 package sherwood.cells.environment;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 public class Environment {
+    private Collection<Organism> organisms = new ArrayList<Organism>();
 
     public void update() {
+        for (Organism organism : organisms) {
+            organism.update(this);
+        }
     }
 }
--- src/sherwood/cells/physics/Engine.java
+++ src/sherwood/cells/physics/Engine.java
@@ -1,14 +1,9 @@
 package sherwood.cells.physics;
 
-import sherwood.cells.Organism;
 import sherwood.cells.environment.Environment;
-
-import java.util.ArrayList;
-import java.util.Collection;
 
 public class Engine {
 	private Environment environment;
-    private Collection<Organism> organisms = new ArrayList<Organism>();
 
 	public Engine(Environment environment) {
 		this.environment = environment;
@@ -16,10 +11,6 @@ public class Engine {
 
 	public void update() {
         environment.update();
-        for (Organism organism : organisms) {
-            organism.update(environment);
-        }
-
     }
 
 	public Environment getEnvironment() {
