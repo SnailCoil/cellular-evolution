56d675b0f48dc0a7af307de5fb86ef7c1913ab8c
SnailCoil <purpleflurm@gmail.com>
2012-09-05 01:00:23 -0600

    Added some logic to ForceVector

--- src/sherwood/cells/physics/ForceVector.java
+++ src/sherwood/cells/physics/ForceVector.java
@@ -1,14 +1,25 @@
 package sherwood.cells.physics;
 
 public class ForceVector {
-    public double x, y;
     public double angle;
     public double magnitude;
 
-    public ForceVector(double x, double y, double angle, double magnitude) {
-        this.x = x;
-        this.y = y;
+    public ForceVector(double angle, double magnitude) {
         this.angle = angle;
         this.magnitude = magnitude;
+    }
+
+    public static ForceVector add(ForceVector vector1, ForceVector vector2) {
+		ForceVector result;
+		if(vector1 == null) {
+			result = vector2;
+		}
+		else if(vector2 == null) {
+			result = vector1;
+		}
+		else {
+			throw new RuntimeException("not implemented");
+		}
+		return result;
     }
 }
